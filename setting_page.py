29aeaf2bf4a13e03b7e7c533522ed52945b2f69b
Serried <contact.rewmb@gmail.com>
2024-10-08 23:03:28 +0700

    setting page
    [WIP] Slider

--- setting_page.py
+++ setting_page.py
@@ -1,14 +1,18 @@
 """selecting game mode"""
 import pygame
+from button import *
 
 def setting_page():
     """return screen for selecting game mode"""
+    
+    from main import main_page
     pygame.display.set_caption("Setting")
     screen = pygame.display.set_mode((1280, 720))
     running = True
 
     brown = 185, 156, 107
     dark_brown = 73, 56, 41
+    bright_brown = 169, 161, 140
 
     while running:
         for event in pygame.event.get():
@@ -18,8 +22,15 @@ def setting_page():
         screen.fill(brown)
 
         text = pygame.font.Font("fonts/Pixelify_Sans/PixelifySans-Medium.ttf", 30)
-        text_screen = text.render("This is screen for setting.", True, dark_brown)
-        screen.blit(text_screen, (400, 300))
+        text_screen = text.render("Settings", True, dark_brown)
+        screen.blit(text_screen, (50, 50))
+
+        pygame.draw.polygon(screen, dark_brown, [(50, 100), (1230, 100), (1230, 110), (50, 110)])
+        pygame.draw.rect(screen, dark_brown, pygame.Rect(175, 150, 900, 100), border_radius=20)
+        pygame.draw.rect(screen, dark_brown, pygame.Rect(175, 300, 900, 100), border_radius=20)
+        pygame.draw.rect(screen, dark_brown, pygame.Rect(175, 450, 900, 100), border_radius=20)
+
+        button("APPLY", 45, 540, 585, 200, 80, brown, bright_brown, main_page)
 
         pygame.display.update()
 
