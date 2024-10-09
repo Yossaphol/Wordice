e12dafc5957ce8c3e1bc208d4c9cbb000a8f9b4c
Alakok77 <top.p1234558@gmail.com>
2024-10-09 08:04:33 +0700

    draw line on game map

--- draw_line.py
+++ draw_line.py
@@ -6,33 +6,37 @@ running = True
 # map
 map_game = pygame.image.load("images/game_map.png")
 map_game = pygame.transform.scale(map_game, (1290, 730))
-
+# 900 420, 980 460
 clock = pygame.time.Clock()
-fps = 30
+fps = 60
 
-f = 0
-g = 0
-
+brown = 95, 65, 22
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            g -= 10
-        if keys[pygame.K_s]:
-            g += 10
-        if keys[pygame.K_a]:
-            f -= 10
-        if keys[pygame.K_d]:
-            f += 10
     screen.blit(map_game, (0, 0))
 
-    pygame.draw.circle(screen, (240,128,128), (f,g), 10)
+    click = pygame.mouse.get_pressed()
+    pos = pygame.mouse.get_pos()
+
+    if click[0] == 1:
+        print(pos[0], pos[1])
+    pygame.draw.line(screen, brown, (900, 420), (980, 460), 5)
+    pygame.draw.line(screen, brown, (980, 380), (1060, 420), 5)
+    pygame.draw.line(screen, brown, (1060, 340), (1140, 380), 5)
+    pygame.draw.line(screen, brown, (1151, 292), (1233, 335), 5)
+    pygame.draw.line(screen, brown, (1150, 290), (1216, 251), 5)
+    pygame.draw.line(screen, brown, (1080, 250), (1140, 217), 5)
+    pygame.draw.line(screen, brown, (1010, 219), (1080, 180), 5)
+    pygame.draw.line(screen, brown, (1010, 218), (950, 188), 5)
+    pygame.draw.line(screen, brown, (939, 259), (877, 225), 5)
+    pygame.draw.line(screen, brown, (819, 258), (880, 294), 5)
+    pygame.draw.line(screen, brown, (819, 257), (760, 290), 5)
+    pygame.draw.line(screen, brown, (763, 232), (709, 263), 5)
+    
 
     pygame.display.update()
     clock.tick(fps)
 
 pygame.quit()
-
