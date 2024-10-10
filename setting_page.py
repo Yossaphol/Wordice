9333b42dbb651a22a21ce86d2f1262ec5dc9c82c
Serried <69156826+Serried@users.noreply.github.com>
2024-10-10 08:33:46 +0700

    Added slide bar

--- setting_page.py
+++ setting_page.py
@@ -13,6 +13,12 @@ def setting_page():
     brown = 185, 156, 107
     silver = 192,192,192
     gainsboro = 220,220,220
+    brown = 185, 156, 107
+    dark_brown = 73, 56, 41
+    bright_brown = 169, 161, 140
+    wheat = 245,222,179
+    lemon = 255, 250, 205
+    black = 0, 0, 0
 
     while running:
         for event in pygame.event.get():
@@ -26,12 +32,28 @@ def setting_page():
         screen.blit(text_screen, (50, 50))
 
         pygame.draw.polygon(screen, dark_brown, [(50, 100), (1230, 100), (1230, 110), (50, 110)])
-        pygame.draw.rect(screen, dark_brown, pygame.Rect(175, 150, 900, 100), border_radius=20)
-        pygame.draw.rect(screen, dark_brown, pygame.Rect(175, 300, 900, 100), border_radius=20)
-        pygame.draw.rect(screen, dark_brown, pygame.Rect(175, 450, 900, 100), border_radius=20)
+        pygame.draw.rect(screen, wheat, pygame.Rect(175, 150, 900, 100), border_radius=20)
+        pygame.draw.rect(screen, wheat, pygame.Rect(175, 300, 900, 100), border_radius=20)
+        pygame.draw.rect(screen, wheat, pygame.Rect(175, 450, 900, 100), border_radius=20)
+        # slide bar
+        pygame.draw.rect(screen, dark_brown, pygame.Rect(450, 195, 600, 10), border_radius=20)
+        pygame.draw.rect(screen, dark_brown, pygame.Rect(450, 345, 600, 10), border_radius=20)
+        pygame.draw.rect(screen, dark_brown, pygame.Rect(450, 495, 600, 10), border_radius=20)
+        # drag and drop in slide bar
+        pygame.draw.rect(screen, bright_brown, pygame.Rect(450, 185, 30, 30), border_radius=30)
+        pygame.draw.rect(screen, bright_brown, pygame.Rect(450, 335, 30, 30), border_radius=30)
+        pygame.draw.rect(screen, bright_brown, pygame.Rect(450, 485, 30, 30), border_radius=30)
 
-        button("APPLY", 30, 540, 585, 150, 60, silver, gainsboro, main_page)
 
+        master = text.render("Master Volume", True, dark_brown)
+        sfx = text.render("SFX Volume", True, dark_brown)
+        music = text.render("Music Volume", True, dark_brown)
+
+        screen.blit(master, (200, 180))
+        screen.blit(sfx, (200, 330))
+        screen.blit(music, (200, 480))
+
+        button("APPLY", 45, 540, 585, 200, 80, wheat, lemon, main_page)
         pygame.display.update()
 
     pygame.quit()
