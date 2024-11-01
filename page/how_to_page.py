d34f55067ca7c2bf5360199d09d44696fd04d2fd
TeaTarius <chayuth.work@gmail.com>
2024-11-01 16:40:01 +0700

    พวกเรามีความสุขมากๆ

--- page/how_to_page.py
+++ page/how_to_page.py
@@ -30,30 +30,52 @@ def how_to_page():
     next_hover = pygame.transform.scale(next_hover, (80, 80))
 
     #รอแก้pap kruf
-    howtoplay = pygame.image.load("images/hottoplay/How To Play.zip - 2.png")
-    howtoplay = pygame.transform.scale(howtoplay, (1290, 730))
+    #howtoplay = pygame.image.load("images/hottoplay/How To Play.zip - 2.png")
+    #howtoplay = pygame.transform.scale(howtoplay, (1290, 730))
 
     #how to image run when click
-    #howto_pic = ["images/hottoplay/How To Play.zip - 3.png","images/hottoplay/How To Play.zip - 4.png",
-    # "images/hottoplay/How To Play.zip - 5.png",images/hottoplay/How To Play.zip - 6.png","images/hottoplay/How To Play.zip - 7.png",
-    # "images/hottoplay/How To Play.zip - 8.png","images/hottoplay/How To Play.zip - 9.png"] hellow worlder
-    #pic_index = 0
+    howto_pic = [
+        "images/hottoplay/How To Play.zip - 3.png",
+        "images/hottoplay/How To Play.zip - 4.png",
+        "images/hottoplay/How To Play.zip - 5.png",
+        "images/hottoplay/How To Play.zip - 6.png",
+        "images/hottoplay/How To Play.zip - 7.png",
+        "images/hottoplay/How To Play.zip - 8.png",
+        "images/hottoplay/How To Play.zip - 9.png"
+    ]
 
-    #current_pic = pygame.image.load(howto_pic[pic_index])
+    pic_index = 0
+
+    current_pic = pygame.image.load(howto_pic[pic_index])
+    current_pic = pygame.transform.scale(current_pic, (1290, 730))
+
 
     while running:
 
         screen.fill(lightblue)
-        screen.blit(howtoplay, (0, 0))
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+
+                if back.get_rect(topleft=(30, 620)).collidepoint(mouse_pos):
+                    main_page()
+                    return
+
+                elif next.get_rect(topleft=(1170, 620)).collidepoint(mouse_pos):
+                    pic_index = (pic_index + 1) % len(howto_pic)
+                    current_pic = pygame.image.load(howto_pic[pic_index])
+                    current_pic = pygame.transform.scale(current_pic, (1290, 730))
+
+
         button_menu(30, 620, 150, 60, back, back_hover, main_page)
-        button_menu(1170, 620, 150, 60, next, next_hover,main_page)
-        
+        button_menu(1170, 620, 150, 60, next, next_hover)
+        screen.blit(current_pic, (0, 0))
+
         pygame.display.update()
 
     pygame.quit()
