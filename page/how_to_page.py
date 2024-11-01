f5f138388efdeeaf2cef01d3fda186c1ac35666d
Jarinya19y <67070019@kmitl.ac.th>
2024-11-01 13:46:21 +0700

    how to test

--- page/how_to_page.py
+++ page/how_to_page.py
@@ -12,6 +12,7 @@ def how_to_page():
     brown = 185, 156, 107
     silver = 192,192,192
     gainsboro = 220,220,220
+    lightblue = 205, 223, 255
 
     mouse = pygame.mouse.get_pressed()
     scrolls = 0
@@ -28,12 +29,19 @@ def how_to_page():
     next = pygame.transform.scale(next, (80, 80))
     next_hover = pygame.transform.scale(next_hover, (80, 80))
 
+    #รอแก้pap kruf
     howtoplay = pygame.image.load("images/hottoplay/How To Play.zip - 2.png")
     howtoplay = pygame.transform.scale(howtoplay, (1290, 730))
 
+    #how to image run when click
+    #howto_pic = ["wait for img url pap krub"]
+    #pic_index = 0
+
+    #current_pic = pygame.image.load(howto_pic[pic_index])
+
     while running:
 
-        screen.fill(brown)
+        screen.fill(lightblue)
         screen.blit(howtoplay, (0, 0))
 
         for event in pygame.event.get():
