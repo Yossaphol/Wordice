d64dc1f37b92b22642fce182d591bed05e5a636e
Alakok77 <top.p1234558@gmail.com>
2024-11-04 11:28:56 +0700

    add dowload file:

---
+++ dowload/main.py
@@ -0,0 +1,140 @@
+"""main page"""
+import pygame
+import pygame.locals
+import sys
+from page.setting_page import setting_page
+from page.how_to_page import how_to_page
+from buttons.menu import button_menu
+from page.game_2_players import game_2_players
+
+pygame.init()
+
+def main_page():
+    """return first page of exiting game"""
+    running = True
+    FPS = 30
+    y = 100
+    y_duck = 50
+    x_cloud1 = 0
+    x_cloud2 = 0
+    down = True
+    down_duck = True
+
+    time = 0
+
+    pygame.mixer.music.load("sounds/sound.mp3")
+    pygame.mixer.music.play(-1)
+
+    pygame.display.set_caption("Bordice")
+
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+
+    # setup images
+    background = pygame.image.load("images/background_test.jpg")
+    logo = pygame.image.load("images/logo3.png")
+    duck = pygame.image.load("images/duck.png")
+    cloud1 = pygame.image.load("images/cloud1.png")
+    cloud2 = pygame.image.load("images/cloud2.png")
+    cloud3 = pygame.image.load("images/cloud3.png")
+    cloud4 = pygame.image.load("images/cloud4.png")
+    dice = pygame.image.load("images/dice.png")
+
+    start = pygame.image.load("images/play.gif")
+    start_hover = pygame.image.load("images/start_hover.png")
+    setting = pygame.image.load("images/setting.png")
+    setting_hover = pygame.image.load("images/setting_hover.png")
+    how = pygame.image.load("images/how.png")
+    how_hover = pygame.image.load("images/how_hover.png")
+    quit = pygame.image.load("images/quit.png")
+    quit_hover = pygame.image.load("images/quit_hover.png")
+    # images tranfrom
+    duck = pygame.transform.scale(duck, (450, 700))
+    logo = pygame.transform.scale(logo, (370, 280))
+    background = pygame.transform.scale(background, (1280, 720))
+    dice = pygame.transform.scale(dice, (200, 200))
+
+    start = pygame.transform.scale(start, (200, 90))
+    start_hover = pygame.transform.scale(start_hover, (200, 90))
+    setting = pygame.transform.scale(setting, (250, 90))
+    setting_hover = pygame.transform.scale(setting_hover, (250, 90))
+    how = pygame.transform.scale(how, (450, 90))
+    how_hover = pygame.transform.scale(how_hover, (450, 90))
+    quit = pygame.transform.scale(quit, (200, 90))
+    quit_hover = pygame.transform.scale(quit_hover, (200, 90))
+
+    # width and height of button
+    start_w = start.get_width()
+    start_h = start.get_height()
+    setting_w = setting.get_width()
+    setting_h = setting.get_height()
+    how_w = how.get_width()
+    how_h = how.get_height()
+    quit_w = quit.get_width()
+    quit_h = quit.get_height()
+
+    # setup color
+    brown = 185, 156, 107
+    bright_brown = 169, 161, 140
+
+    # run page
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.blit(background, (0,0))
+        # cloud
+        screen.blit(cloud1, (x_cloud1, 10))
+        screen.blit(cloud2, (x_cloud2 + 800, 80))
+        screen.blit(cloud3, (x_cloud2 - 400, 150))
+        screen.blit(cloud4, (x_cloud1 + 500, -100))
+        screen.blit(cloud1, (x_cloud2 - 1300, 200))
+        screen.blit(cloud2, (x_cloud1 - 2100, 100))
+        screen.blit(cloud3, (x_cloud1 - 1400, 150))
+        screen.blit(cloud4, (x_cloud2 - 3000 , -100))
+
+        screen.blit(logo, (75, 10))
+        screen.blit(duck, (650, y_duck))
+        screen.blit(dice, (850, y))
+
+        # cloud transition
+        if time < 3500:
+            time += 1
+            x_cloud1 += 1
+            x_cloud2 += 2
+        else:
+            time = 0
+            x_cloud1 = -1000
+            x_cloud2 = -1000
+
+        # duck transition
+        if down_duck:
+            y_duck += 0.5
+            if y_duck >= 50:
+                down_duck = False
+        else:
+            y_duck -= 0.5
+            if y_duck < 40:
+                down_duck = True
+
+        if down:
+            y += 1
+            if y >= 140:
+                down = False
+        else:
+            y -= 1
+            if y < 110:
+                down = True
+
+        # button on screen
+        button_menu(185, 280, start_w, start_h, start, start_hover, game_2_players)
+        button_menu(155, 370,setting_w, setting_h, setting, setting_hover, setting_page)
+        button_menu(60, 460, how_w, how_h, how, how_hover, how_to_page)
+        button_menu(185, 550, quit_w, quit_h, quit, quit_hover, sys.exit)
+
+        pygame.display.update()
+        clock.tick(FPS)
+    pygame.quit()
+
+main_page()
---
+++ dowload/wordle_sys/wordle.py
@@ -0,0 +1,332 @@
+import pygame
+import sys
+from pygame.locals import *
+from wordle_sys.vocab import *
+from object.player_profile import profile
+from object.turn_player import turn_player
+from buttons.button import text_object
+from object.winner import *
+pygame.init()
+
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+grey = (211, 211, 211)
+black = (0, 0, 0)
+green = (0, 255, 0)
+red = 255,0,0
+
+small = pygame.font.Font("fonts/Pixelify.ttf", 30)
+font = pygame.font.Font("fonts/Pixelify.ttf", 40)
+big_font = pygame.font.Font("fonts/Pixelify.ttf", 60)
+
+def show_description(descript, x_start, y_start):
+    """Show the multiline of word description"""
+
+    brown = 185, 156, 107
+    font = pygame.font.Font("fonts/Pixelify.ttf", 20)
+
+    words = descript.split(" ")
+
+    x = x_start
+    y = y_start
+    line_height = font.get_height() + 4
+
+    for word in words:
+        word_t = font.render(word, True, brown)
+        word_width = word_t.get_width()
+
+        if x + word_width <= 1000:
+            screen.blit(word_t, (x, y))
+            x += word_width + 2
+        else:
+            y += line_height
+            x = x_start
+            screen.blit(word_t, (x, y))
+            x += word_width + 2
+
+def check_guess(turns, word, user_guess, screen):
+    """function for check the word that player guess and return win or not"""
+    guess_color_code = [grey for _ in range(len(word))]
+    point = 0
+    word_checklist = list(word)
+
+    for x in range(len(word)):
+        if user_guess[x] == word[x]:
+            guess_color_code[x] = green
+            point += 2
+            word_checklist[x] = None
+
+    for x in range(len(word)):
+        if guess_color_code[x] != green and user_guess[x] in word_checklist:
+            guess_color_code[x] = yellow
+            point += 1
+            word_checklist[word_checklist.index(user_guess[x])] = None
+
+    if guess_color_code == [green for _ in range(len(word))]:
+        point += 3
+
+    spacing = 0
+    for x in range(len(word)):
+        rendered_char = font.render(user_guess[x], True, black)
+        pygame.draw.rect(screen, guess_color_code[x], pygame.Rect(260 + spacing, 130 + (turns * 60), 50, 50))
+        screen.blit(rendered_char, (270 + spacing, 130 + (turns * 60)))
+        spacing += 60
+
+    return guess_color_code == [green for _ in range(len(word))], point
+
+def wordle(turn, total_turn, point_player1, point_player2, final_point_a, final_point_b):
+    """return wordle gameplay"""
+    height = 720
+    width = 1280
+    dark_brown = 73, 56, 41
+
+    burly_wood = 222,184,135
+    light_steel_blue  = 176,196,222
+    silver = 192,192,192
+    grey = 128,128,128
+    brown = 185, 156, 107
+    dark_golden_rod = 184,134,11
+    golden_rod = 218,165,32
+    wheat = 245,222,179
+    beige = 245,245,220
+    powder_blue = 176,224,230
+
+    background = pygame.image.load("images/background_game.jpg")
+    background = pygame.transform.scale(background, (1280, 720))
+
+    map_game = pygame.image.load("images/game_map_2.png")
+    map_game = pygame.transform.scale(map_game, (1290, 730))
+
+    player_1 = pygame.image.load("images/player_1.png")
+    player_2 = pygame.image.load("images/player_2.png")
+    player_1 = pygame.transform.scale(player_1, (230, 230))
+    player_2 = pygame.transform.scale(player_2, (230, 230))
+
+    bonus = pygame.image.load("images/bonus_duck.png")
+    bonus = pygame.transform.scale(bonus, (250, 300))
+    
+    qeustion_mark = pygame.image.load("images/question.png")
+    qeustion_mark = pygame.transform.scale(qeustion_mark, (180, 280))
+
+    FPS = 30
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((width, height))
+
+    screen.blit(background, (0, 0))
+
+    word, description = vocab_random()
+    word = word.upper()
+    guess = ""
+    turns = 0
+    win = False
+    all_guesses = []
+
+    print(word)
+    add = 0
+    winner = turn
+
+    while True:
+
+        screen.blit(map_game, (0, 0))
+
+        profile(130, 630, point_player1,  70, brown, "PLAYER 1", player_1)
+        profile(1150, 120, point_player2, 70, brown, "PLAYER 2", player_2)
+
+        pygame.draw.rect(screen, grey, (243, 113, 803, 503), 0, 0, 20, 20, 20, 20)
+        pygame.draw.rect(screen, burly_wood, (240, 110, 800, 500), 0, 0, 20, 20, 20, 20)
+
+        pygame.draw.rect(screen, wheat, (330, 30, 550, 60), 0, 0, 40, 40, 40, 40)
+        describes = pygame.font.Font("fonts/Pixelify.ttf", 50)
+        turn_remain = describes.render("TURN REMAINING : ", True, dark_brown)
+        remain = describes.render(f"{20 - total_turn}", True, red)
+        screen.blit(turn_remain, (350, 30))
+        screen.blit(remain, (800, 30))
+
+
+        for x in range(len(word)):
+            for y in range(6):
+                pygame.draw.rect(screen, grey, pygame.Rect(260 + (x * 60), 130 + (y * 60), 50, 50), 2)
+
+        for i, g in enumerate(all_guesses):
+            check_guess(i, word, g, screen)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+
+            if turns < 6 and not win:  # Prevent input after 6 turns or if the player already won
+                if event.type == pygame.KEYDOWN:
+                    if event.key == K_BACKSPACE:
+                        guess = guess[:-1]
+                    elif event.key == K_RETURN:
+                        if len(guess) == len(word):
+                            all_guesses.append(guess)
+                            win, add = check_guess(turns, word, guess, screen)
+                            turns += 1
+                            guess = ""
+
+                            if turn == "player1":
+                                point_player1 += add
+                                final_point_a += add
+                                add = 0
+                                if not win:
+                                    turn = "player2"
+                            elif turn == "player2":
+                                point_player2 += add
+                                final_point_b += add
+                                add = 0
+                                if not win:
+                                    turn = "player1"
+
+                    elif event.unicode.isalpha() and len(guess) < len(word):
+                        guess += event.unicode.upper()
+
+        your_text = small.render("GUESSING :", True, dark_golden_rod)
+        render_guess = font.render(guess, True, dark_brown)
+        screen.blit(your_text, (280, 500))
+        screen.blit(render_guess, (440, 490))
+        
+        txt_upper = small.render("type the word", True, dark_brown)
+        txt_middle = small.render("that you think", True, dark_brown)
+        txt_lower = small.render("it will be correct", True, dark_brown)
+        screen.blit(txt_upper, (700, 440))
+        screen.blit(txt_middle, (700, 480))
+        screen.blit(txt_lower, (700, 520))
+        
+        screen.blit(qeustion_mark, (730, 150))
+
+        if turn == "player1":
+            turn_player(90, 640)
+        elif turn == "player2":
+            turn_player(1120, 130)
+
+        # out of turn remaining
+        if total_turn == 20:
+            return True, False, winner, total_turn, point_player1, point_player2, final_point_a, final_point_b
+
+        # win and move
+        elif win:
+            winner = turn
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+
+            sound = pygame.mixer.Sound("sounds/click.MP3")
+
+            pygame.draw.rect(screen, beige, (640, 120, 380, 480), 0, 0, 20, 20, 20, 20)
+            pygame.draw.rect(screen, powder_blue, (660, 250, 343, 180), 0, 0, 20, 20, 20, 20)
+
+            if 940 > mouse[0] > 740 and 490 > mouse[1] > 450:
+                pygame.draw.rect(screen, "BLACK", (743, 453, 200,40), 0, 0, 30 ,30, 30, 30)
+                pygame.draw.rect(screen, silver, (740,450,200,40), 0, 0, 30 ,30, 30, 30)
+
+                if click[0] == 1:
+                    sound.play()
+                    total_turn += 1
+                    if winner == "player1":
+                        point_player1 = 0
+                    else:
+                        point_player2 = 0
+                    return True, False, winner, total_turn, point_player1, point_player2, final_point_a, final_point_b
+
+            else:
+                pygame.draw.rect(screen, "BLACK", (743, 453, 200,40), 0, 0, 30 ,30, 30, 30)
+                pygame.draw.rect(screen, light_steel_blue, (740,450,200,40), 0, 0, 30 ,30, 30, 30)
+
+            win_text = big_font.render("You Win!", True, green)
+            meaning_text = font.render("MEANING :", True, dark_brown)
+            screen.blit(meaning_text, (750, 200))
+            screen.blit(win_text, (720, 130))
+
+            show_description(description, 690, 270)
+
+            smallText = pygame.font.Font("fonts/Pixelify.ttf", 30)
+            text_surface, text_rect = text_object("NEXT", smallText)
+            text_rect.center = ((840), (470))
+            screen.blit(text_surface, text_rect)
+
+        # when not correct
+        elif turns == 6 and not win:
+            if winner == "player1":
+                winner = "player2"
+            else:
+                winner = "player1"
+
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+
+            sound = pygame.mixer.Sound("sounds/click.MP3")
+
+            pygame.draw.rect(screen, beige, (640, 120, 380, 480), 0, 0, 20, 20, 20, 20)
+            pygame.draw.rect(screen, powder_blue, (660, 250, 343, 180), 0, 0, 20, 20, 20, 20)
+
+            if 940 > mouse[0] > 740 and 490 > mouse[1] > 450:
+                pygame.draw.rect(screen, "BLACK", (743, 453, 200,40), 0, 0, 30 ,30, 30, 30)
+                pygame.draw.rect(screen, silver, (740,450,200,40), 0, 0, 30 ,30, 30, 30)
+
+                if click[0] == 1:
+                    sound.play()
+                    total_turn += 1
+                    return False, True, winner, total_turn, point_player1, point_player2, final_point_a, final_point_b
+
+            else:
+                pygame.draw.rect(screen, "BLACK", (743, 453, 200,40), 0, 0, 30 ,30, 30, 30)
+                pygame.draw.rect(screen, light_steel_blue, (740,450,200,40), 0, 0, 30 ,30, 30, 30)
+
+            win_text = big_font.render("You Lose!", True, red)
+            meaning_text = font.render("THE WORD IS :", True, dark_brown)
+            answer = font.render(word, True, dark_golden_rod)
+            screen.blit(win_text, (700, 130))
+            screen.blit(meaning_text, (715, 200))
+            screen.blit(answer, (780, 250))
+
+            show_description(description, 680, 300)
+
+            smallText = pygame.font.Font("fonts/Pixelify.ttf", 30)
+            text_surface, text_rect = text_object("NEXT", smallText)
+            text_rect.center = ((840), (470))
+            screen.blit(text_surface, text_rect)
+
+
+        # bonus move
+        elif point_player1 >= 20 or point_player2 >= 20:
+
+            if turn == "player1":
+                point_player1 = 0
+            else:
+                point_player2 = 0
+
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+
+            sound = pygame.mixer.Sound("sounds/click.MP3")
+
+            pygame.draw.rect(screen, beige, (640, 120, 380, 480), 0, 0, 20, 20, 20, 20)
+            pygame.draw.rect(screen, powder_blue, (660, 250, 343, 180), 0, 0, 20, 20, 20, 20)
+            screen.blit(bonus, (700, 200))
+
+            if 940 > mouse[0] > 740 and 490 > mouse[1] > 450:
+                pygame.draw.rect(screen, "BLACK", (743, 453, 200,40), 0, 0, 30 ,30, 30, 30)
+                pygame.draw.rect(screen, silver, (740,450,200,40), 0, 0, 30 ,30, 30, 30)
+
+                if click[0] == 1:
+                    sound.play()
+                    total_turn += 1
+                    return True, False, winner, total_turn, point_player1, point_player2, final_point_a, final_point_b
+
+            else:
+                pygame.draw.rect(screen, "BLACK", (743, 453, 200,40), 0, 0, 30 ,30, 30, 30)
+                pygame.draw.rect(screen, light_steel_blue, (740,450,200,40), 0, 0, 30 ,30, 30, 30)
+
+            win_text = big_font.render("Bonus", True, red)
+            meaning_text = small.render("You can move free!!", True, dark_brown)
+            screen.blit(win_text, (750, 130))
+            screen.blit(meaning_text, (690, 200))
+
+
+            smallText = pygame.font.Font("fonts/Pixelify.ttf", 30)
+            text_surface, text_rect = text_object("NEXT", smallText)
+            text_rect.center = ((840), (470))
+            screen.blit(text_surface, text_rect)
+
+        pygame.display.update()
+        clock.tick(FPS)
